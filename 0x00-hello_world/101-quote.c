b3f42b230ef1aab6912d934ef0e532dfd362fe7e
geebabee <manchagrace@gmail.com>
2023-03-18 06:41:23 -0700

    101-quote.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A c program that prints a like to a standard error
- * Return: 1 when successful
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", std
-out);
-	return (1);
+write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof ("and that piece ofart is useful\" - Dorakorpar, 2015-10-19"));
+return (1);
 }
