45157aaf86bb1cf8d68aeedbfaa7c369517e9782
geebabee <manchagrace@gmail.com>
2023-03-18 05:58:44 -0700

    101-quote.c task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A c program that prints a like to a standard error
+ * Return: 1 when successful
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", std
+out);
+	return (1);
+}
