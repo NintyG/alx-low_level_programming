d137d7ba5b8323e95cdc6fe99623a6a0bddf7283
geebabee <manchagrace@gmail.com>
2023-03-26 04:23:21 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-
 /**
  * main - main function
  *
  * Return: always 0
  */
-
 int main(void)
-
 {
-	int p, q;
-
-	for (p = 0 ; p <= 99 ; p++)
-	{
-		for (q = 0 ; q <= 99 ; q++)
-		{
-			if (p < q)
-			{
-				putchar((p / 10) + 48);
-				putchar((p % 10) + 48);
-				putchar(' ');
+        int p, q;
+        for (p = 0 ; p <= 99 ; p++)
+        {
+                for (q = 0 ; q <= 99 ; q++)
+                {
+                        if (p < q)
+                        {
+                                putchar((p / 10) + 48);
+                                putchar((p % 10) + 48);
+                                putchar(' ');
 				putchar((q / 10) + 48);
 				putchar((q % 10) + 48);
 				if (p != 98 || q != 99)
@@ -32,3 +28,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
+}
