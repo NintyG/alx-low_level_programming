d8dcada2e4530df2578e86fc906ac7e8fb398015
geebabee <manchagrace@gmail.com>
2023-03-26 04:36:39 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-        int p, q;
-        for (p = 0 ; p <= 99 ; p++)
-        {
-                for (q = 0 ; q <= 99 ; q++)
-                {
-                        if (p < q)
-                        {
-                                putchar((p / 10) + 48);
-                                putchar((p % 10) + 48);
-                                putchar(' ');
+	int p, q;
+
+	for (p = 0 ; p <= 99 ; p++)
+	{
+		for (q = 0 ; q <= 99 ; q++)
+		{
+			if (p < q)
+			{
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
+				putchar(' ');
 				putchar((q / 10) + 48);
 				putchar((q % 10) + 48);
 				if (p != 98 || q != 99)
