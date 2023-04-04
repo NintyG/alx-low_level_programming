a7190d1eea34fd3f3d9ac9571d5800d1ec6dd62f
geebabee <manchagrace@gmail.com>
2023-04-03 23:33:46 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
-*_strspn - Gets the length of a prefix
-* @s: string to be tested
-* @accept: string to find
-* Return: returns the number of bytes
+ * _strchr - prints found c
+ * @s: pointer to char
+ * @c: char params to found
+ * Return: *S
 */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int x, y;
-	unsigned int c = 0;
 
-	for (x = 0; s[x] != 0; x++)
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
 	{
-		if (s[x] == ' ')
-			break;
-		for (y = 0; accept[y] != 0; y++)
+		if (*s == c)
 		{
-			if (s[x] == accept[y])
-				c++;
+			return (s);
 		}
+		s++;
 	}
-	return (c);
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
 }
