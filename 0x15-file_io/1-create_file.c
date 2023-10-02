a82714fc5843d0360121a2de2e5f200853b061d8
NintyG <manchagrace@gmail.com>
2023-10-02 13:02:02 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file - Creates a file.
- * @filename: Pointer to the name of the file you create.
- * @text_content: Pointer to a string to write to the file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
